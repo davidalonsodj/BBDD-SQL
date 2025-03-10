932dace2d347d15ef463e55e7a0adf5ac57ac75d
davidalonsodj <davidalonsodj@gmail.com>
2025-03-10 16:48:21 +0100

    Update IntroBBDD.sql

---
+++ IntroBBDD.sql
@@ -0,0 +1,20 @@
+--RECUPERAR VUELOS STATUS ON TIME
+SELECT flight_id, flight_no, status 
+FROM flights 
+WHERE status = 'On Time';
+--BOOKINGS RUBLITOS
+SELECT *
+FROM bookings
+WHERE total_amount >= 100000;
+--MODELOS AVIONES
+SELECT *
+FROM
+aircraft_data;
+--BOING 737
+SELECT flight_id, flight_no
+FROM flights
+WHERE aircraft_code = 737;
+--DETALLE TICKETS
+SELECT *
+FROM tickets
+WHERE passenger_name LIKE 'IRINA%';
